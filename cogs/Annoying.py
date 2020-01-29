084cbe56ad1bd91a92d4b6f4dcd95976b40dd077
samohtemmitt <samohtemmitt@gmail.com>
2020-01-28 22:01:04 -0500

    fix typo in hours calc

--- cogs/Annoying.py
+++ cogs/Annoying.py
@@ -12,7 +12,7 @@ class Annoying(commands.Cog):
         if len(message) > 150:
             await ctx.send("Sorry, that message is too long. Messages must be less than 150 characters.")
         await ctx.send("Timer set; in {0} hours I will ping {1.name} here to tell them {2}".format(hours,user,message))
-        await asyncio.sleep(round(360 * hours))
+        await asyncio.sleep(round(3600 * hours))
         await ctx.send("{0.mention}, {1.author.name}#{1.author.discriminator} asked me {2} hours ago to tell you {3}".format(user,ctx,hours,message))
 
 def setup(client):
