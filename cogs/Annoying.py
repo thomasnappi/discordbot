286620d1f7b96cd47e80aa7dcd1b7de72ec1fbaf
samohtemmitt <samohtemmitt@gmail.com>
2020-01-28 21:52:38 -0500

    working nag function

--- cogs/Annoying.py
+++ cogs/Annoying.py
@@ -7,13 +7,13 @@ class Annoying(commands.Cog):
 
     @commands.command(name="remind",aliases=["nag","bother"],pass_context=True)
     @commands.guild_only()
-    async def remind(self, ctx, user: discord.Member, hours: float, message: str):
+    async def remind(self, ctx, user: discord.Member, hours: float, *, message: str):
         """ Pings the user in this channel with the specified message after a certain number of hours (fractions supported)"""
         if len(message) > 150:
             await ctx.send("Sorry, that message is too long. Messages must be less than 150 characters.")
         await ctx.send("Timer set; in {0} hours I will ping {1.name} here to tell them {2}".format(hours,user,message))
         await asyncio.sleep(round(360 * hours))
-        await ctx.send("{0.mention}, {1.author.name}#{1.author.discriminator} asked me {2} hours ago to tell you {3}".format(user,ctx.author,hours,message))
+        await ctx.send("{0.mention}, {1.author.name}#{1.author.discriminator} asked me {2} hours ago to tell you {3}".format(user,ctx,hours,message))
 
 def setup(client):
     client.add_cog(Annoying(client))
