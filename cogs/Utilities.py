5c8b0d317562f74ec26aa63278c9cd6494571726
samohtemmitt <samohtemmitt@gmail.com>
2020-09-05 02:09:30 -0400

    should have fixed the emoji adding

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -215,7 +215,7 @@ class Utilities(commands.Cog):
             await sent.add_reaction('👍')
             await sent.add_reaction('👎')
         else:
-            nums = ['one','two','three','four','five','six','seven','eight','nine']
+            nums = ['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣']
             for i in range(options):
                 await sent.add_reaction(nums[i])
 
